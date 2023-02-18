1e0b8845718b6cc9b70183c49e81a5d3e7d7bbb5
Lukas Schmid <schmluk@mavt.ethz.ch>
2023-02-17 23:11:09 -0500

    WIP: Initial new design layout

---
+++ assets/js/build-publication.js
@@ -0,0 +1,52 @@
+// Tools to build the publication tables. Called in publications.html.
+function buildTable(publications, tablename) {
+  var tbody = document.getElementById(tablename).getElementsByTagName('tbody')[0];
+  for (let i = 0; i < publications.length; i++) {
+    addTableRow(publications[i], tbody);
+  }
+}
+
+function addTableRow(publication, tbody) {
+  // Build the row content for the publication.
+  var text = ""
+
+  // Lead image.
+  text += "<td style='width: 25%; padding-right: 1em; padding-bottom: 1em'><a href='" + publication.venue.link + "' target='_blank'><img src='images/publications/" + publication.venue.image + "' style='width: 100%' alt=" + publication.venue.title + "'></a></td>";
+
+  // Authors.
+  text += "<td style='width: 75%; vertical-align: top; padding-bottom: 1em; line-height: 1.4em'>";
+  for (let i = 0; i < publication.authors.length; i++) {
+    if (i > 0) {
+      if (publication.authors.length > 2) {
+        text += ", ";
+      } else {
+        text += " ";
+      }
+    }
+    if (i == publication.authors.length - 1) {
+      text += "and ";
+    }
+
+    var name_check = publication.authors[i].toLowerCase();
+      if (name_check == "l. schmid" || name_check == "lukas schmid" || name_check == "schmid, l." || name_check == "schmid, lukas") {
+        text += "<strong>" + publication.authors[i] + "</strong>";
+      } else {
+        text += publication.authors[i];
+      }
+  }
+
+  // Title and reference.
+  text += "<br><span style='font-weight: bold; color: rgb(80, 100, 200);'>" + publication.title + "</span><br>" + publication.venue.name + ", " + publication.pages + ", " + publication.year + ".<br>";
+
+
+  // Links.
+  for (let i = 0; i < publication.links.length; i++) {
+    text += "[<a href=" + publication.links[i].link + ">" + publication.links[i].name + "</a>]&nbsp;";
+  }
+
+  text += "</div></td>"
+
+  // Add the data to the table.
+  tbody.insertRow().innerHTML = text;
+}
+
