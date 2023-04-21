00c7e80de40bac534120bb0272766f451b25e184
Schmluk <lschmid@mit.edu>
2023-04-21 11:55:06 -0400

    Update news, code, and publications

--- publications/preprints.js
+++ publications/preprints.js
@@ -1,19 +1,29 @@
-const preprintVenues = {"Arxiv" :{
+const preprintVenues = {
+  "Arxiv": {
     "name": "ArXiv Preprint",
     "link": "https://arxiv.org/",
     "image": "arxiv.png"
-}
+  }
 };
 
 const pubPreprints = [
-{
-  "authors": ["L. Schmid", "M. N. Cheema", "V. Reijgwart", "R. Siegwart", "F. Tombari", "C. Cadena"],
-  "title": "SC-Explorer: Incremental 3D Scene Completion for Safe and Efficient Exploration Mapping and Planning",
-  "venue": preprintVenues["Arxiv"],
-  "year": "2022",
-  "pages": "",
-  "links": [{ "name": "ArXiv", "link": "https://arxiv.org/abs/2208.08307"}],
-  "sharedfirst": false
-}
+  {
+    "authors": ["L. Schmid", "O. Andersson", "A. Sulser", "P. Pfreundschuh", "R. Siegwart"],
+    "title": "Dynablox: Real-time Detection of Diverse Dynamic Objects in Complex Environments",
+    "venue": preprintVenues["Arxiv"],
+    "year": "2023",
+    "pages": "",
+    "links": [{ "name": "ArXiv", "link": "https://arxiv.org/abs/2304.10049" }, { "name": "Code", "link": "https://github.com/ethz-asl/dynablox" }],
+    "sharedfirst": false
+  },
+  {
+    "authors": ["L. Schmid", "M. N. Cheema", "V. Reijgwart", "R. Siegwart", "F. Tombari", "C. Cadena"],
+    "title": "SC-Explorer: Incremental 3D Scene Completion for Safe and Efficient Exploration Mapping and Planning",
+    "venue": preprintVenues["Arxiv"],
+    "year": "2022",
+    "pages": "",
+    "links": [{ "name": "ArXiv", "link": "https://arxiv.org/abs/2208.08307" }],
+    "sharedfirst": false
+  }
 ]
 
