6631a6c854b0ef109b5c4d779d3bbef1483f1f62
lschmid <lschmid@mit.edu>
2023-06-07 09:43:00 -0400

    update news

--- publications/news.js
+++ publications/news.js
@@ -2,6 +2,13 @@
 // News thumb: WxH ~ 1x0.7
 const pubNews = [
   {
+    "title": "We presented our works on 3D-VSGs [<a href='https://arxiv.org/abs/2209.07896' />Paper</a>] [<a href='https://github.com/ethz-asl/3d_vsg' />Code</a>] and SC-Explorer [<a href='https://arxiv.org/abs/2208.08307' />Paper</a>] at <a href='https://www.icra2023.org/' />ICRA 2023</a>!",
+    "date": "6/2023",
+    "image": "icra23.png",
+    "thumb": "icra23_t.jpg",
+    "caption": "We presented our works on 3D-VSGs [<a href='https://arxiv.org/abs/2209.07896' />Paper</a>] [<a href='https://github.com/ethz-asl/3d_vsg' />Code</a>] and SC-Explorer [<a href='https://arxiv.org/abs/2208.08307' />Paper</a>] at <a href='https://www.icra2023.org/' />ICRA 2023</a>!"
+  },
+  {
     "title": "We're excited to announce <a href='https://github.com/ethz-asl/dynablox' />Dynablox</a>: our new online mapping-based moving object detection framework! [<a href='https://arxiv.org/abs/2304.10049'/>Preprint</a>] [<a href='https://www.youtube.com/watch?v=NA13fRWlqRQ'>Video</a>] [<a href='https://github.com/ethz-asl/dynablox'/>Code</a>]",
     "date": "4/2023",
     "image": "../code/dynablox.gif",
@@ -14,53 +21,53 @@ const pubNews = [
     "image": "eth_medal.jpg",
     "thumb": "eth_medal_t.jpg",
     "caption": "My student René Zurbrügg is awarded the <a href='https://ethz.ch/en/the-eth-zurich/education/awards/eth-medal/outstanding-master-theses.html' />ETH Medal</a>, Congrats!"
-  },  
+  },
   {
-  "title": "Our paper proposing <a href='https://arxiv.org/abs/2209.07896' />3D Variable Scene Graphs</a> is accepted to <a href='https://www.icra2023.org/' />ICRA'23</a>! [<a href='https://arxiv.org/abs/2209.07896'/>Paper</a>] [<a href='https://github.com/ethz-asl/3d_vsg'/>Code</a>]",
-  "date": "3/2023",
-  "image": "3d_vsg.png",
-  "thumb": "3d_vsg_t.png",
-  "caption": "Our paper proposing <a href='https://arxiv.org/abs/2209.07896' />3D Variable Scene Graphs</a> for Long-term Semantic Scene Change Prediction is accepted to <a href='https://www.icra2023.org/' />ICRA'23</a>! [<a href='https://arxiv.org/abs/2209.07896'/>Paper</a>] [<a href='https://github.com/ethz-asl/3d_vsg'/>Code</a>]"
-},
-{
-  "title": "I joined the <a href='https://web.mit.edu/sparklab/' />MIT-SPARK Lab</a> as a Postdoctoral Research Fellow!",
-  "date": "2/2023",
-  "image": "mit.jpg",
-  "thumb": "mit_t.jpg",
-  "caption": "I'm excited to share that have joined the <a href='https://web.mit.edu/sparklab/' />MIT-SPARK Lab</a> as a postdoctoral fellow working with <a href='https://lucacarlone.mit.edu/'>Prof. Luca Carlone</a>!"
-},
-{
-  "title": "I have been awarded a <a href='https://www.snf.ch/en'/>Swiss National Science Foundation</a> <a href='https://data.snf.ch/grants/grant/214489'/>Career Grant</a>!",
-  "date": "12/2022",
-  "image": "snf.png",
-  "thumb": "snf.png",
-  "caption": "I have been awarded a <a href='https://www.snf.ch/en'/>Swiss National Science Foundation</a> <a href='https://data.snf.ch/grants/grant/214489' />Postdoc.Mobility Career Grant</a>!"
-},
-{
-  "title": "I presented our work on <a href='https://ieeexplore.ieee.org/document/9807401'/>Learning Exploration</a> at <a href='https://iros2022.org/'/>IROS 2022</a>! [<a href='https://ieeexplore.ieee.org/document/9807401'/>Paper</a>] [<a href='https://www.youtube.com/watch?v=Hj5yI8VtlXk'/>Video</a>] [<a href='https://github.com/ethz-asl/cvae_exploration_planning'/>Code</a>]",
-  "date": "10/2022",
-  "image": "iros22.jpg",
-  "thumb": "iros22_t.jpg",
-  "caption": "Great discussions at <a href='https://iros2022.org/'/>IROS 2022</a> about our <a href='https://ieeexplore.ieee.org/document/9807401'/>work</a>!"
-},
-{
-  "title": "I successfully defended my <a href='https://www.research-collection.ethz.ch/handle/20.500.11850/506422'/>PhD Thesis</a> on Scene Understanding in Active Perception and Mapping!",
-  "date": "10/2022",
-  "image": "phd.jpg",
-  "thumb": "phd_t.jpg",
-  "caption": "I successfully defended my <a href='https://www.research-collection.ethz.ch/handle/20.500.11850/506422'/>PhD Thesis</a>!"
-},
-{
-  "title": "I presented our work on <a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a> at <a href='https://icra2022.org'/>ICRA 2022</a>! [<a href='https://ieeexplore.ieee.org/document/9811877'/>Paper</a>] [<a href='https://www.youtube.com/watch?v=A7o2Vy7_TV4'/>Video</a>] [<a href='https://github.com/ethz-asl/panoptic_mapping'/>Code</a>]",
-  "date": "5/2022",
-  "image": "icra22.jpg",
-  "thumb": "icra22_t.jpg",
-  "caption": "I presented our work on <a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a> at <a href='https://icra2022.org'/>ICRA 2022</a>! [<a href='https://ieeexplore.ieee.org/document/9811877'/>Paper</a>] [<a href='https://www.youtube.com/watch?v=A7o2Vy7_TV4'/>Video</a>] [<a href='https://github.com/ethz-asl/panoptic_mapping'/>Code</a>]"
-},
-{
-  "title": "<a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a>, our latest volumetric mapping system is released as <a href='https://github.com/ethz-asl/panoptic_mapping'/>open-source</a>!",
-  "date": "1/2022",
-  "image": "pan.png",
-  "thumb": "pan_t.png",
-  "caption": "<a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a>, our latest volumetric mapping system is released as <a href='https://github.com/ethz-asl/panoptic_mapping'/>open-source</a>!"
-}]
+    "title": "Our paper proposing <a href='https://arxiv.org/abs/2209.07896' />3D Variable Scene Graphs</a> is accepted to <a href='https://www.icra2023.org/' />ICRA'23</a>! [<a href='https://arxiv.org/abs/2209.07896'/>Paper</a>] [<a href='https://github.com/ethz-asl/3d_vsg'/>Code</a>]",
+    "date": "3/2023",
+    "image": "3d_vsg.png",
+    "thumb": "3d_vsg_t.png",
+    "caption": "Our paper proposing <a href='https://arxiv.org/abs/2209.07896' />3D Variable Scene Graphs</a> for Long-term Semantic Scene Change Prediction is accepted to <a href='https://www.icra2023.org/' />ICRA'23</a>! [<a href='https://arxiv.org/abs/2209.07896'/>Paper</a>] [<a href='https://github.com/ethz-asl/3d_vsg'/>Code</a>]"
+  },
+  {
+    "title": "I joined the <a href='https://web.mit.edu/sparklab/' />MIT-SPARK Lab</a> as a Postdoctoral Research Fellow!",
+    "date": "2/2023",
+    "image": "mit.jpg",
+    "thumb": "mit_t.jpg",
+    "caption": "I'm excited to share that have joined the <a href='https://web.mit.edu/sparklab/' />MIT-SPARK Lab</a> as a postdoctoral fellow working with <a href='https://lucacarlone.mit.edu/'>Prof. Luca Carlone</a>!"
+  },
+  {
+    "title": "I have been awarded a <a href='https://www.snf.ch/en'/>Swiss National Science Foundation</a> <a href='https://data.snf.ch/grants/grant/214489'/>Career Grant</a>!",
+    "date": "12/2022",
+    "image": "snf.png",
+    "thumb": "snf.png",
+    "caption": "I have been awarded a <a href='https://www.snf.ch/en'/>Swiss National Science Foundation</a> <a href='https://data.snf.ch/grants/grant/214489' />Postdoc.Mobility Career Grant</a>!"
+  },
+  {
+    "title": "I presented our work on <a href='https://ieeexplore.ieee.org/document/9807401'/>Learning Exploration</a> at <a href='https://iros2022.org/'/>IROS 2022</a>! [<a href='https://ieeexplore.ieee.org/document/9807401'/>Paper</a>] [<a href='https://www.youtube.com/watch?v=Hj5yI8VtlXk'/>Video</a>] [<a href='https://github.com/ethz-asl/cvae_exploration_planning'/>Code</a>]",
+    "date": "10/2022",
+    "image": "iros22.jpg",
+    "thumb": "iros22_t.jpg",
+    "caption": "Great discussions at <a href='https://iros2022.org/'/>IROS 2022</a> about our <a href='https://ieeexplore.ieee.org/document/9807401'/>work</a>!"
+  },
+  {
+    "title": "I successfully defended my <a href='https://www.research-collection.ethz.ch/handle/20.500.11850/506422'/>PhD Thesis</a> on Scene Understanding in Active Perception and Mapping!",
+    "date": "10/2022",
+    "image": "phd.jpg",
+    "thumb": "phd_t.jpg",
+    "caption": "I successfully defended my <a href='https://www.research-collection.ethz.ch/handle/20.500.11850/506422'/>PhD Thesis</a>!"
+  },
+  {
+    "title": "I presented our work on <a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a> at <a href='https://icra2022.org'/>ICRA 2022</a>! [<a href='https://ieeexplore.ieee.org/document/9811877'/>Paper</a>] [<a href='https://www.youtube.com/watch?v=A7o2Vy7_TV4'/>Video</a>] [<a href='https://github.com/ethz-asl/panoptic_mapping'/>Code</a>]",
+    "date": "5/2022",
+    "image": "icra22.jpg",
+    "thumb": "icra22_t.jpg",
+    "caption": "I presented our work on <a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a> at <a href='https://icra2022.org'/>ICRA 2022</a>! [<a href='https://ieeexplore.ieee.org/document/9811877'/>Paper</a>] [<a href='https://www.youtube.com/watch?v=A7o2Vy7_TV4'/>Video</a>] [<a href='https://github.com/ethz-asl/panoptic_mapping'/>Code</a>]"
+  },
+  {
+    "title": "<a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a>, our latest volumetric mapping system is released as <a href='https://github.com/ethz-asl/panoptic_mapping'/>open-source</a>!",
+    "date": "1/2022",
+    "image": "pan.png",
+    "thumb": "pan_t.png",
+    "caption": "<a href='https://ieeexplore.ieee.org/document/9811877'/>Panoptic Mapping</a>, our latest volumetric mapping system is released as <a href='https://github.com/ethz-asl/panoptic_mapping'/>open-source</a>!"
+  }]
---
+++ publications/workshops.js
@@ -0,0 +1,15 @@
+
+const pubWorkshops = [{
+  "authors": ["M. Müller", "J. Lim", "L. Schmid", "H. Blum", "W. Stürzl", "A. Gawel", "R. Siegwart", "R. Triebel"],
+  "title": "Interactive OAISYS: A photorealistic terrain simulation for robotics research",
+  "venue": {
+    "name": "IEEE ICRA 2022 Workshop on Releasing Robots into the Wild: Simulations, Benchmarks, and Deployment ",
+    "link": "https://www.dynsyslab.org/releasing-robots-into-the-wild-workshop/",
+    "image": "icra22.png"
+  },
+  "year": "2022",
+  "pages": "Philadelphia(PA), USA, May",
+  "links": [{ "name": "Paper", "link": "https://elib.dlr.de/188049/" }, { "name": "Code", "link": "https://github.com/DLR-RM/oaisys" }],
+  "sharedfirst": false
+}
+]
