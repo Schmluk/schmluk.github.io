074b5d6fb3abde3383774e1bd0b538b013ade73b
Schmluk <lschmid@mit.edu>
2023-09-26 01:32:18 -0400

    Update journal refs

--- publications/journals.js
+++ publications/journals.js
@@ -17,7 +17,7 @@ const pubJournals = [
     "title": "Dynablox: Real-time Detection of Diverse Dynamic Objects in Complex Environments",
     "venue": journalVenues["RAL"],
     "year": "2023",
-    "pages": "",
+    "pages": "vol. 8, no. 10, pp. 6259-6266",
     "links": [{ "name": "Journal", "link": "https://ieeexplore.ieee.org/document/10218983" },{ "name": "ArXiv", "link": "https://arxiv.org/abs/2304.10049" }, { "name": "Code", "link": "https://github.com/ethz-asl/dynablox" }, { "name": "Video", "link": "https://www.youtube.com/watch?v=NA13fRWlqRQ" }],
     "sharedfirst": false
   },
@@ -26,7 +26,7 @@ const pubJournals = [
   "title": "Embodied Active Domain Adaptation for Semantic Segmentation via Informative Path Planning",
   "venue": journalVenues["RAL"],
   "year": "2022",
-  "pages": "7 (4), pp. 8691-8698",
+  "pages": " vol. 7 no. 4, pp. 8691-8698",
   "links": [{ "name": "Journal", "link": "https://ieeexplore.ieee.org/document/9816133" }, { "name": "ArXiv", "link": "https://arxiv.org/abs/2203.00549" }, { "name": "Code", "link": "https://github.com/ethz-asl/active_learning_for_segmentation" }, { "name": "Video", "link": "https://www.youtube.com/watch?v=FeFPEdZzT3w" }],
   "sharedfirst": false
 },
@@ -35,7 +35,7 @@ const pubJournals = [
   "title": "Fast and Compute-efficient Sampling-based Local Exploration Planning via Distribution Learning",
   "venue": journalVenues["RAL"],
   "year": "2022",
-  "pages": "7 (2), pp. 7810-7817",
+  "pages": "vol. 7 no. 2, pp. 7810-7817",
   "links": [{ "name": "Journal", "link": "https://ieeexplore.ieee.org/document/9807401" }, { "name": "ArXiv", "link": "https://arxiv.org/abs/2202.13715" }, { "name": "Code", "link": "https://github.com/ethz-asl/cvae_exploration_planning" }, { "name": "Video", "link": "https://www.youtube.com/watch?v=Hj5yI8VtlXk" }],
   "sharedfirst": false
 },
@@ -44,7 +44,7 @@ const pubJournals = [
   "title": "A Unified Approach for Autonomous Volumetric Exploration of Large Scale Environments under Severe Odometry Drift",
   "venue": journalVenues["RAL"],
   "year": "2021",
-  "pages": "6 (3), pp. 4504-4511",
+  "pages": "vol. 6 no. 3, pp. 4504-4511",
   "links": [{ "name": "Journal", "link": "https://ieeexplore.ieee.org/document/9387110" }, { "name": "ArXiv", "link": "https://arxiv.org/abs/2010.09859" }, { "name": "Code", "link": "https://github.com/ethz-asl/glocal_exploration" }, { "name": "Video", "link": "https://www.youtube.com/watch?v=WInjZvoCDCM" }],
   "sharedfirst": false
 },
@@ -53,7 +53,7 @@ const pubJournals = [
   "title": "An Efficient Sampling-based Method for Online Informative Path Planning in Unknown Environments",
   "venue": journalVenues["RAL"],
   "year": "2020",
-  "pages": "5 (2), pp. 1500-1507",
+  "pages": "vol. 5 no. 2, pp. 1500-1507",
   "links": [{ "name": "Journal", "link": "https://ieeexplore.ieee.org/document/8968434" }, { "name": "ArXiv", "link": "https://arxiv.org/abs/1909.09548" }, { "name": "Code", "link": "https://github.com/ethz-asl/mav_active_3d_planning" }, { "name": "Video", "link": "https://www.youtube.com/watch?v=lEadqJ1_8Do" }],
   "sharedfirst": false
 },
@@ -62,7 +62,7 @@ const pubJournals = [
   "title": "A Spatio Temporal Spectral Framework for plant Stress Phenotyping",
   "venue": journalVenues["PlantMethods"],
   "year": "2019",
-  "pages": "15 (1), pp. 1-18",
+  "pages": "vol. 15 no. 1, pp. 1-18",
   "links": [{ "name": "Journal", "link": "https://plantmethods.biomedcentral.com/articles/10.1186/s13007-019-0398-8" }, { "name": "Code", "link": "https://github.com/ethz-asl/plant_stress_phenotyping" },],
   "sharedfirst": false
 }
