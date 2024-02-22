74f3a494485106ed282d7d7b31ff8990bdbcfc09
Schmluk <lschmid@mit.edu>
2024-02-22 17:07:46 -0500

    Add Khronos

--- publications/preprints.js
+++ publications/preprints.js
@@ -7,6 +7,15 @@ const preprintVenues = {
 };
 const pubPreprints = [
   {
+    "authors": ["L. Schmid", "M. Abate", "Y. Chang", "L. Carlone"],
+    "title": "Khronos: A Unified Approach for Spatio-Temporal Metric-Semantic SLAM in Dynamic Environments",
+    "venue": preprintVenues["Arxiv"],
+    "year": "2024",
+    "pages": "",
+    "links": [{ "name": "ArXiv", "link": "https://arxiv.org/abs/2402.13817" }, {"name": "Code", "link": "https://github.com/MIT-SPARK/Khronos"}],
+    "sharedfirst": false
+  },
+  {
     "authors": ["L. Schmid", "M. N. Cheema", "V. Reijgwart", "R. Siegwart", "F. Tombari", "C. Cadena"],
     "title": "SC-Explorer: Incremental 3D Scene Completion for Safe and Efficient Exploration Mapping and Planning",
     "venue": preprintVenues["Arxiv"],
